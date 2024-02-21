9bc21613ee70816b7440d354c73141b59a6a078e
joshco15 <99153246+joshco15@users.noreply.github.com>
2024-02-21 01:50:55 -0500

    Update Part1.sql

--- Part1.sql
+++ Part1.sql
@@ -1,82 +1,86 @@
-CREATE TABLE MotionPicture (
-    id INTEGER NOT NULL,
-    name CHAR(20),
+CREATE TABLE motion_picture (
+    mpid INTEGER NOT NULL,
+    name CHAR(50),
     rating FLOAT,
     production CHAR(20),
     budget INTEGER,
-    PRIMARY KEY(mp_id)
+    PRIMARY KEY(mpid)
 );
 
-CREATE TABLE User (
-    email CHAR(30) NOT NULL,
-    name CHAR(20),
+CREATE TABLE user (
+    uemail CHAR(40) NOT NULL,
+    name CHAR(30),
     age INTEGER,
-    PRIMARY KEY(email)
+    PRIMARY KEY(uemail)
 );
 
-CREATE TABLE Likes (
-    uemail INT NOT NULL,
+CREATE TABLE likes (
+    uemail CHAR(40) NOT NULL,
     mpid INT NOT NULL,
     PRIMARY KEY (uemail, mpid),
-    FOREIGN KEY(uemail) REFERENCES User,
-    FOREIGN KEY(mpid) REFERENCES MotionPicture
-);
-
-CREATE TABLE Movie (
-    mpid INTEGER NOT NULL,
-    boxoffice_collection INTEGER,
-    PRIMARY KEY(mpid),
-    FOREIGN KEY(mpid) REFERENCES MotionPicture
-);
-
-CREATE TABLE Series (
-    mpid INTEGER NOT NULL,
-    season_count INTEGER,
-    PRIMARY KEY(mpid),
-    FOREIGN KEY(mpid) REFERENCES MotionPicture
-);
-
-CREATE TABLE People (
-    id INTEGER NOT NULL,
-    name CHAR(20),
-    nationality CHAR(20),
-    dob DATE,
-    gender CHAR(20),
-    PRIMARY KEY(id)
-);
-
-CREATE TABLE Role (
-    mpid INTEGER,
-    pid INTEGER,
-    role_name CHAR(20),
-    FOREIGN KEY(mpid) REFERENCES MotionPicture,
-    FOREIGN KEY(pid) REFERENCES People
-);
-
-CREATE TABLE Award (
-    mpid INTEGER NOT NULL,
-    pid INTEGER NOT NULL,
-    award_name CHAR(20) NOT NULL,
-    award_year CHAR(20) NOT NULL,
-    PRIMARY KEY (mpid, pid, award_name, award_year),
-    FOREIGN KEY(mpid) REFERENCES MotionPicture,
-    FOREIGN KEY(pid) REFERENCES People
-);
-
-CREATE TABLE Genre (
-    mpid INTEGER,
-    genre_name CHAR(20),
-    FOREIGN KEY(mpid) REFERENCES MotionPicture
-);
-
-CREATE TABLE Location (
-    mpid INTEGER NOT NULL,
-    zip INTEGER,
-    city CHAR(20),
-    country CHAR(20),
-    PRIMARY KEY(mpid, zip),
-    FOREIGN KEY(mpid) REFERENCES MotionPicture,
+    FOREIGN KEY(uemail) REFERENCES user,
+    FOREIGN KEY(mpid) REFERENCES motion_picture,
     ON DELETE CASCADE
 );
 
+CREATE TABLE movie (
+    mpid INTEGER NOT NULL,
+    boxoffice_collection INTEGER,
+    PRIMARY KEY(mpid),
+    FOREIGN KEY(mpid) REFERENCES motion_picture,
+    ON DELETE CASCADE
+);
 
+CREATE TABLE series (
+    mpid INTEGER NOT NULL,
+    season_count INTEGER,
+    PRIMARY KEY(mpid),
+    FOREIGN KEY(mpid) REFERENCES motion_picture,
+    ON DELETE CASCADE
+);
+
+CREATE TABLE people (
+    pid INTEGER NOT NULL,
+    name CHAR(30),
+    nationality CHAR(20),
+    dob DATE,
+    gender CHAR(20),
+    PRIMARY KEY(pid)
+);
+
+CREATE TABLE role (
+    mpid INTEGER,
+    pid INTEGER,
+    role_name CHAR(20),
+    FOREIGN KEY(mpid) REFERENCES motion_picture,
+    FOREIGN KEY(pid) REFERENCES people,
+    ON DELETE CASCADE
+);
+
+CREATE TABLE award (
+    mpid INTEGER NOT NULL,
+    pid INTEGER NOT NULL,
+    award_name CHAR(20) NOT NULL,
+    award_year INTa NOT NULL,
+    PRIMARY KEY (mpid, pid, award_name, award_year),
+    FOREIGN KEY(mpid) REFERENCES motion_picture,
+    FOREIGN KEY(pid) REFERENCES people,
+    ON DELETE CASCADE
+);
+
+CREATE TABLE genre (
+    mpid INTEGER,
+    genre_name CHAR(20),
+    FOREIGN KEY(mpid) REFERENCES motion_picture,
+    ON DELETE CASCADE
+);
+
+CREATE TABLE location (
+    mpid INTEGER NOT NULL,
+    zip INTEGER NOT NULL,
+    city CHAR(20),
+    country CHAR(20),
+    PRIMARY KEY(mpid, zip),
+    FOREIGN KEY(mpid) REFERENCES motion_picture,
+    ON DELETE CASCADE
+);
